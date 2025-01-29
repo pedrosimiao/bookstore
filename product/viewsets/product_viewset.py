607f41f1eba998bbd48471751a5b750bd499647b
pedrosimiao <pedrolumiere@gmail.com>
2025-01-29 00:40:59 -0300

    Config Authentication, adapt viewsets & tests for permissions & pagination

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -7,5 +7,5 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        return Product.objects.all()
+        return Product.objects.all().order_by('id')
     
